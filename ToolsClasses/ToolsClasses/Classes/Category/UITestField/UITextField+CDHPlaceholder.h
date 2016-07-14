999b7ad460e4ca121c6c48cdcb124a4a56a08235
chendehao <chendehao@macintosh-2.local>
2016-07-14 19:39:42 +0800

    添加 textField 文本输入站位文字的颜色和光标的分类

---
+++ ToolsClasses/ToolsClasses/Classes/Category/UITestField/UITextField+CDHPlaceholder.h
@@ -0,0 +1,15 @@
+//
+//  UITextField+CDHPlaceholder.h
+//
+//
+//  Created by chendehao on 15/5/14.
+//  Copyright © 2015年 陈德豪. All rights reserved.
+//  专门用来设置 textField 文本数入框的中站位字符的文本颜色和光标的颜色分类
+
+#import <UIKit/UIKit.h>
+
+@interface UITextField (CDHPlaceholder)
+
+@property UIColor *placeholderColor;
+
+@end
---
+++ ToolsClasses/ToolsClasses/Classes/Category/UITestField/UITextField+CDHPlaceholder.m
@@ -0,0 +1,58 @@
+//
+//  UITextField+CDHPlaceholder.m
+//  BuDeJie
+//
+//  Created by chendehao on 16/7/14.
+//  Copyright © 2016年 陈德豪. All rights reserved.
+//
+
+#import "UITextField+CDHPlaceholder.h"
+#import <objc/message.h>
+
+@implementation UITextField (CDHPlaceholder)
+
++ (void)load
+{
+    Method setPlaceholderMethod = class_getInstanceMethod(self, @selector(setPlaceholder:));
+    
+    Method setCDHPlaceholderMethod = class_getInstanceMethod(self, @selector(setCDH_Placeholder:));
+    
+    method_exchangeImplementations(setPlaceholderMethod, setCDHPlaceholderMethod);
+}
+
+
+// setPlaceholder : setCDH_Placeholder
+// 设置占位文字,并且设置占位文字颜色
+- (void)setCDH_Placeholder:(NSString *)placeholder
+{
+    // 设置占位文字
+    [self setCDH_Placeholder:placeholder];
+    
+    // 设置占位文字颜色
+    [self setPlaceholderColor:self.placeholderColor];
+    
+}
+
+// 设置占位文字颜色
+- (void)setPlaceholderColor:(UIColor *)placeholderColor
+{
+    // 1.把占位文字颜色先保存
+    objc_setAssociatedObject(self, @"placeholderColor", placeholderColor, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    
+    // 2.等真正设置占位文字的时候,在去设置占位文字颜色
+    
+    // 获取占位文字控件
+    UILabel *placeholderLabel = [self valueForKey:@"placeholderLabel"];
+    
+    // 拿到控件去设置颜色
+    placeholderLabel.textColor = placeholderColor;
+    
+}
+
+// 返回占位文字颜色
+- (UIColor *)placeholderColor
+{
+    return objc_getAssociatedObject(self, @"placeholderColor");
+}
+
+@end
