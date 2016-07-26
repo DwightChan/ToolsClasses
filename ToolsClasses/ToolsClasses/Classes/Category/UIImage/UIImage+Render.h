22dada876adcc2d71f0b0333d20bd3e0575fcd69
chendehao <958689927@qq.com>
2016-07-26 19:20:09 +0800

    给裁剪圆形图片添加方法

--- ToolsClasses/ToolsClasses/Classes/Category/UIImage/UIImage+Render.h
+++ ToolsClasses/ToolsClasses/Classes/Category/UIImage/UIImage+Render.h
@@ -4,7 +4,7 @@
 //
 //  Created by 陈德豪 on 16/7/8.
 //  Copyright © 2016年 陈德豪. All rights reserved.
-//
+//  图片裁剪的
 
 #import <UIKit/UIKit.h>
 
@@ -17,7 +17,7 @@
  *
  *  @return 返回没有被渲染的图片
  */
-+ (UIImage *)imageWithOriginal:(UIImage *)image;
++ (UIImage *)cdh_imageWithOriginal:(UIImage *)image;
 
 
 /**
@@ -25,6 +25,13 @@
  *
  *  @return 返回一个圆形图片
  */
-- (UIImage *)circleImage;
+- (UIImage *)cdh_circleImage;
+
+/**
+ *  将原图裁剪为指定大小的圆角图片
+ *
+ *  @return 放回一个指定大小的圆形图片
+ */
+- (UIImage *)cdh_circleImageWithSize:(CGSize)size;
 
 @end
--- ToolsClasses/ToolsClasses/Classes/Category/UIImage/UIImage+Render.m
+++ ToolsClasses/ToolsClasses/Classes/Category/UIImage/UIImage+Render.m
@@ -10,13 +10,13 @@
 
 @implementation UIImage (Render)
 
-+ (UIImage *)imageWithOriginal:(UIImage *)image
++ (UIImage *)cdh_imageWithOriginal:(UIImage *)image
 {
     // 返回一个没有渲染图片
     return  [image imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
 }
 
-- (UIImage *)circleImage{
+- (UIImage *)cdh_circleImage{
     
     // 裁剪图片: 图形上下文
     // 1.开启图形上下文
@@ -34,6 +34,27 @@
     UIGraphicsEndImageContext();
     
     return image;
+}
+
+- (UIImage *)cdh_circleImageWithSize:(CGSize)size{
+    
+    // 裁剪图片: 图形上下文
+    // 1.开启图形上下文
+    // scale:比例因素 点:像素比例 0:自动识别比例因素
+    UIGraphicsBeginImageContextWithOptions(size, NO, 0);
+    // 2.描述圆形裁剪路径
+    UIBezierPath *clipPath = [UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, size.width, size.height)];
+    // 3.设置为裁剪区域
+    [clipPath addClip];
+    // 4.画图片  (CGPointZero 原图的大小)
+    [self drawAtPoint:CGPointZero];
+    // 5.取出图片
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    // 6.关闭上下文
+    UIGraphicsEndImageContext();
+    
+    return image;
+
 }
 
 @end
